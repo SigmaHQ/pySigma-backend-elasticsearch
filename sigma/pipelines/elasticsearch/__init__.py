cbdbb7d2268d2d8b73d19b9fc8b1a67389eabdbd
Thomas Patzke <thomas@patzke.org>
2022-07-10 15:35:48 +0200

    Added Windows ECS pipeline

--- sigma/pipelines/elasticsearch/__init__.py
+++
@@ -1,2 +0,0 @@
-from .elasticsearch import elasticsearch_example
-# TODO: add all pipelines that should be exposed to the user of your backend in the import statement above.
--- tests/test_pipelines_elasticsearch.py
+++ tests/test_pipelines_elasticsearch.py
@@ -1,5 +1,21 @@
 import pytest
 from sigma.backends.elasticsearch import ElasticsearchQueryStringBackend
-#from sigma.pipelines.elasticsearch import # TODO: import pipeline functions
+from sigma.pipelines.elasticsearch.ecs import ecs_windows
+from sigma.collection import SigmaCollection
 
-# TODO: import tests for all implemented pipelines and contained transformations
+def test_ecs_windows():
+    assert ElasticsearchQueryStringBackend(ecs_windows()).convert(
+        SigmaCollection.from_yaml(f"""
+            title: Test
+            status: test
+            logsource:
+                product: windows
+                service: security
+            detection:
+                sel:
+                    EventID: 123
+                    Image: test.exe
+                    TestField: test
+                condition: sel
+        """)
+    ) == ['winlog.channel:"Security" AND event.code:123 AND process.executable:"test.exe" AND winlog.event_data.TestField:"test"']
