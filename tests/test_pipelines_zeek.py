c718ae526dc6a00edf131b46b4af23466ce80300
Thomas Patzke <thomas@patzke.org>
2022-07-21 14:20:00 +0200

    Raw Zeek JSON logs pipeline

--- tests/test_pipelines_zeek.py
+++ tests/test_pipelines_zeek.py
@@ -1,36 +1,31 @@
 import pytest
 from sigma.backends.elasticsearch import ElasticsearchQueryStringBackend
-from sigma.pipelines.elasticsearch.zeek import ecs_zeek_beats, ecs_zeek_corelight
+from sigma.pipelines.elasticsearch.zeek import ecs_zeek_beats, ecs_zeek_corelight, zeek_raw
 from sigma.collection import SigmaCollection
 
-def test_ecs_zeek_beats():
-    assert ElasticsearchQueryStringBackend(ecs_zeek_beats()).convert(
-        SigmaCollection.from_yaml(f"""
-            title: Test
-            status: test
-            logsource:
-                category: proxy
-            detection:
-                sel:
-                    method: POST
-                    cs-host: test.invalid
-                    username: testuser
-                condition: sel
-        """)
-    ) == ['event.dataset:"zeek.http" AND http.request.method:"POST" AND url.domain:"test.invalid" OR destination.domain:"test.invalid" AND zeek.\\*.username:"testuser"']
+@pytest.fixture
+def sigma_rule():
+    return SigmaCollection.from_yaml(f"""
+        title: Test
+        status: test
+        logsource:
+            category: proxy
+        detection:
+            sel:
+                cs-method: POST
+                cs-host: test.invalid
+                username: testuser
+            condition: sel
+    """)
 
-def test_ecs_zeek_corelight():
-    assert ElasticsearchQueryStringBackend(ecs_zeek_corelight()).convert(
-        SigmaCollection.from_yaml(f"""
-            title: Test
-            status: test
-            logsource:
-                category: proxy
-            detection:
-                sel:
-                    method: POST
-                    cs-host: test.invalid
-                    username: testuser
-                condition: sel
-        """)
-    ) == ['event.dataset:"zeek.http" AND http.request.method:"POST" AND url.domain:"test.invalid" OR destination.domain:"test.invalid" AND zeek.\\*.username:"testuser"']
+def test_ecs_zeek_beats(sigma_rule):
+    assert ElasticsearchQueryStringBackend(ecs_zeek_beats()).convert(sigma_rule) == \
+        ['event.dataset:"zeek.http" AND http.request.method:"POST" AND url.domain:"test.invalid" OR destination.domain:"test.invalid" AND zeek.\\*.username:"testuser"']
+
+def test_ecs_zeek_corelight(sigma_rule):
+    assert ElasticsearchQueryStringBackend(ecs_zeek_corelight()).convert(sigma_rule) == \
+        ['event.dataset:"zeek.http" AND http.request.method:"POST" AND url.domain:"test.invalid" OR destination.domain:"test.invalid" AND zeek.\\*.username:"testuser"']
+
+def test_zeek_raw(sigma_rule):
+    assert ElasticsearchQueryStringBackend(zeek_raw()).convert(sigma_rule) == \
+        ['@stream:"http" AND method:"POST" AND host:"test.invalid" AND username:"testuser"']
