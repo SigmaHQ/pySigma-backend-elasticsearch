c073d6aab80567150f65240da6e21950c67d9fd7
Hendrik <andurin@process-zero.de>
2023-10-06 22:24:41 +0200

    Feature: Elasticsearch EQL Backend

--- sigma/backends/elasticsearch/__init__.py
+++ sigma/backends/elasticsearch/__init__.py
@@ -1,5 +1,7 @@
 from .elasticsearch_lucene import LuceneBackend
+from .elasticsearch_eql import EqlBackend
 
 backends = {
     "elasticsearch": LuceneBackend,
+    "elasticsearch-eql": EqlBackend,
 }
