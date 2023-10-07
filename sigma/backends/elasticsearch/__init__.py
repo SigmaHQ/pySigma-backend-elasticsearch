9fbbcb446b09d0b78e400149673100e5146e9c2f
Hendrik <andurin@process-zero.de>
2023-10-07 14:33:07 +0200

    Removed imports
    Thanks to the autoloader

--- sigma/backends/elasticsearch/__init__.py
+++
@@ -1,7 +0,0 @@
-from .elasticsearch_lucene import LuceneBackend
-from .elasticsearch_eql import EqlBackend
-
-backends = {
-    "elasticsearch": LuceneBackend,
-    "elasticsearch-eql": EqlBackend,
-}
