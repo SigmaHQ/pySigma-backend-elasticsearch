8bc0e35d6476bc36d7ebac622807deeb8167a5de
Leo Tsaousis <leo.tsaou@gmail.com>
2024-01-29 10:11:22 +0000

    Introducing a new pipeline for native Kubernetes audit logs (#42)
    The newly-added `sigma/pipelines/elasticsearch/kubernetes.py` pipeline allows the authoring of Sigma rules operating upon native [Audit logs](https://kubernetes.io/docs/tasks/debug/debug-cluster/audit/) with the following new logsource:
    ```yml
    logsource:
        product: kubernetes
        service: audit_logs
    ```
    
    ...to be converted into valid ELK/Lucene queries, as per the [ELK Kubernetes Integration](https://docs.elastic.co/en/integrations/kubernetes).
    
    In brief, the pipeline performs the following steps:
    - translation of user-friendly fields such as `verb` to their fully-qualified counterparts (`kubernetes.audit.verb`)
    - prepends queries with a `kubernetes.audit.kind:Event` condition to improve query performance by reducing event scope
    - drops fields that are not set by the integration under certain circumstances, to align with user expectation
    
    The new pipeline works in tandem with the new set of Kubernetes Sigma rules developed ([repo](https://github.com/LAripping/sigma/tree/kubernetes-cpattacks))

---
+++ sigma/pipelines/elasticsearch/kubernetes.py
@@ -0,0 +1,62 @@
+from sigma.processing.transformations import FieldMappingTransformation, AddConditionTransformation, DropDetectionItemTransformation
+from sigma.processing.conditions import LogsourceCondition, IncludeFieldCondition, MatchStringCondition
+from sigma.processing.pipeline import ProcessingItem, ProcessingPipeline
+
+def ecs_kubernetes() -> ProcessingPipeline:
+    return ProcessingPipeline(
+        name="Elastic Common Schema (ECS) Kubernetes audit log mappings",
+        priority=30,
+        allowed_backends=("elasticsearch", "eql", "lucene"),
+        items=[
+            ProcessingItem(
+                identifier="index_condition",
+                transformation=AddConditionTransformation(
+                    conditions={ 'kubernetes.audit.kind': 'Event' },
+                    template=False
+                ),
+                rule_conditions=[
+                    LogsourceCondition(product='kubernetes',service='audit_logs'),
+                ]
+            ),
+            ProcessingItem(
+                identifier='field_mapping',
+                transformation=FieldMappingTransformation(
+                    mapping={
+                        'verb': ['kubernetes.audit.verb'],
+                        'apiGroup':['kubernetes.audit.objectRef.apiGroup'],
+                        'resource':['kubernetes.audit.objectRef.resource'],
+                        'subresource':['kubernetes.audit.objectRef.subresource'],
+                        'namespace':['kubernetes.audit.objectRef.namespace'],
+                        'capabilities':['kubernetes.audit.requestObject.spec.containers.securityContext.capabilities.add'],
+                        'hostPath':['kubernetes.audit.requestObject.spec.volumes.hostPath']
+                    }
+                )
+            ),
+            ProcessingItem(
+                identifier='drop_default_apigroup',
+                transformation=DropDetectionItemTransformation(),
+                field_name_conditions=[
+                    IncludeFieldCondition(fields=[
+                        'apiGroup',
+                        'kubernetes.audit.objectRef.apiGroup'
+                    ])
+                ],
+                detection_item_conditions=[
+                    MatchStringCondition(cond='any',pattern="^$")
+                ]
+            ),
+            ProcessingItem(
+                identifier='drop_empty_subresource',
+                transformation=DropDetectionItemTransformation(),
+                field_name_conditions=[
+                    IncludeFieldCondition(fields=[
+                        'subresource',
+                        'kubernetes.audit.objectRef.subresource'
+                    ])
+                ],
+                detection_item_conditions=[
+                    MatchStringCondition(cond='any',pattern="^$")
+                ]
+            )  
+        ]
+    )
