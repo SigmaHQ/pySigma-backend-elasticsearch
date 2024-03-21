efb69c8a91c531df15b4f54ea332772b18fac510
frack113 <62423083+frack113@users.noreply.github.com>
2024-03-21 18:43:03 +0100

    Run Black

--- tests/test_pipelines_kubernetes.py
+++ tests/test_pipelines_kubernetes.py
@@ -5,8 +5,10 @@ from sigma.rule import SigmaRule
 
 
 def test_ecs_kubernetes():
-    assert LuceneBackend(ecs_kubernetes()).convert(
-        SigmaCollection.from_yaml("""
+    assert (
+        LuceneBackend(ecs_kubernetes()).convert(
+            SigmaCollection.from_yaml(
+                """
             title: Test
             status: test
             logsource:
@@ -17,13 +19,20 @@ def test_ecs_kubernetes():
                   verb: create
                   resource: pods
                 condition: selection
-        """)
-    ) == ['kubernetes.audit.kind:Event AND (kubernetes.audit.verb:create AND kubernetes.audit.objectRef.resource:pods)']
+        """
+            )
+        )
+        == [
+            "kubernetes.audit.kind:Event AND (kubernetes.audit.verb:create AND kubernetes.audit.objectRef.resource:pods)"
+        ]
+    )
 
 
 def test_ecs_kubernetes_apigroup():
-    assert LuceneBackend(ecs_kubernetes()).convert(
-        SigmaCollection.from_yaml("""
+    assert (
+        LuceneBackend(ecs_kubernetes()).convert(
+            SigmaCollection.from_yaml(
+                """
             title: Test
             status: test
             logsource:
@@ -35,12 +44,20 @@ def test_ecs_kubernetes_apigroup():
                   apiGroup: authorization.k8s.io
                   resource: selfsubjectrulesreviews
                 condition: selection
-        """)
-    ) == ["kubernetes.audit.kind:Event AND (kubernetes.audit.verb:create AND kubernetes.audit.objectRef.apiGroup:authorization.k8s.io AND kubernetes.audit.objectRef.resource:selfsubjectrulesreviews)"]
+        """
+            )
+        )
+        == [
+            "kubernetes.audit.kind:Event AND (kubernetes.audit.verb:create AND kubernetes.audit.objectRef.apiGroup:authorization.k8s.io AND kubernetes.audit.objectRef.resource:selfsubjectrulesreviews)"
+        ]
+    )
+
 
 def test_ecs_kubernetes_capabilities():
-    assert LuceneBackend(ecs_kubernetes()).convert(
-        SigmaCollection.from_yaml("""
+    assert (
+        LuceneBackend(ecs_kubernetes()).convert(
+            SigmaCollection.from_yaml(
+                """
             title: Test
             status: test
             logsource:
@@ -52,12 +69,19 @@ def test_ecs_kubernetes_capabilities():
                   resource: pods
                   capabilities: "*"
                 condition: selection
-        """)
-    ) == ["kubernetes.audit.kind:Event AND (kubernetes.audit.verb:create AND kubernetes.audit.objectRef.resource:pods AND kubernetes.audit.requestObject.spec.containers.securityContext.capabilities.add:*)"]
+        """
+            )
+        )
+        == [
+            "kubernetes.audit.kind:Event AND (kubernetes.audit.verb:create AND kubernetes.audit.objectRef.resource:pods AND kubernetes.audit.requestObject.spec.containers.securityContext.capabilities.add:*)"
+        ]
+    )
 
     def test_ecs_kubernetes_subresource():
-        assert LuceneBackend(ecs_kubernetes()).convert(
-        SigmaCollection.from_yaml("""
+        assert (
+            LuceneBackend(ecs_kubernetes()).convert(
+                SigmaCollection.from_yaml(
+                    """
             title: Test
             status: test
             logsource:
@@ -69,12 +93,19 @@ def test_ecs_kubernetes_capabilities():
                   resource: pods
                   subresource: exec
                 condition: selection
-        """)
-    ) == ["kubernetes.audit.kind:Event AND (kubernetes.audit.verb:create AND kubernetes.audit.objectRef.resource:pods AND kubernetes.audit.objectRef.subresource:exec)"]
+        """
+                )
+            )
+            == [
+                "kubernetes.audit.kind:Event AND (kubernetes.audit.verb:create AND kubernetes.audit.objectRef.resource:pods AND kubernetes.audit.objectRef.subresource:exec)"
+            ]
+        )
 
 
 def test_ecs_kubernetes_fields():
-    rule = ecs_kubernetes().apply(SigmaRule.from_yaml("""
+    rule = ecs_kubernetes().apply(
+        SigmaRule.from_yaml(
+            """
             title: Test
             status: test
             logsource:
@@ -89,6 +120,10 @@ def test_ecs_kubernetes_fields():
             fields:
                 - verb
                 - hostPath
-        """)
-                               )
-    assert rule.fields == ["kubernetes.audit.verb", "kubernetes.audit.requestObject.spec.volumes.hostPath"]
+        """
+        )
+    )
+    assert rule.fields == [
+        "kubernetes.audit.verb",
+        "kubernetes.audit.requestObject.spec.volumes.hostPath",
+    ]
