75c1513aa41254cee637cae94c95c271b30cd20f
Leo Tsaousis <leo.tsaou@gmail.com>
2024-08-26 07:42:46 +0100

    Export kubernetes pipeline and mention in doc (#68)

--- sigma/pipelines/elasticsearch/__init__.py
+++ sigma/pipelines/elasticsearch/__init__.py
@@ -1,5 +1,6 @@
 from .windows import ecs_windows, ecs_windows_old
 from .zeek import ecs_zeek_beats, ecs_zeek_corelight, zeek_raw
+from .kubernetes import ecs_kubernetes
 
 pipelines = {
     "ecs_windows": ecs_windows,
@@ -7,4 +8,5 @@ pipelines = {
     "ecs_zeek_beats": ecs_zeek_beats,
     "ecs_zeek_corelight": ecs_zeek_corelight,
     "zeek": zeek_raw,
+    "ecs_kubernetes": ecs_kubernetes, 
 }
