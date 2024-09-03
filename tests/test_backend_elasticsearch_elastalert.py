2dc4eb31c2a7d54dff84c8107903b0e747cec2c0
Chris Barros <chris.barros@hacknowledge.ch>
2024-09-03 10:15:09 +0200

    feat(elastalert): add test on unimplemented temporal ordered correlation rules

--- tests/test_backend_elasticsearch_elastalert.py
+++ tests/test_backend_elasticsearch_elastalert.py
@@ -101,6 +101,7 @@ max_threshold: 10
 type: metric_aggregation"""
     )
 
+
 def test_elastalert_change_severity(elastalert_backend: ElastalertBackend):
     rule = SigmaCollection.from_yaml(
         """
@@ -176,86 +177,6 @@ max_threshold: 10
 type: metric_aggregation"""
     )
 
-def test_elastalert_and_expression(elastalert_backend: ElastalertBackend):
-    rule = SigmaCollection.from_yaml(
-        """
-            title: Test
-            status: test
-            logsource:
-                category: test_category
-                product: test_product
-            detection:
-                sel:
-                    fieldA: value1
-                    fieldB: value2
-                condition: sel
-        """
-    )
-
-    assert elastalert_backend.convert(rule)[0] == (
-        """description: 
-index: *
-filter:
-- query:
-    query_string:
-      query: fieldA:value1 AND fieldB:value2
-priority: 1"""
-    )
-
-
-def test_elastalert_and_expression_empty_string(elastalert_backend: ElastalertBackend):
-    rule = SigmaCollection.from_yaml(
-        """
-            title: Test
-            status: test
-            logsource:
-                category: test_category
-                product: test_product
-            detection:
-                sel:
-                    fieldA: value1
-                    fieldB: ''
-                condition: sel
-        """
-    )
-
-    assert elastalert_backend.convert(rule)[0] == (
-        """description: 
-index: *
-filter:
-- query:
-    query_string:
-      query: fieldA:value1 AND fieldB:""
-priority: 1"""
-    )
-
-
-def test_elastalert_or_expression(elastalert_backend: ElastalertBackend):
-    rule = SigmaCollection.from_yaml(
-        """
-            title: Test
-            status: test
-            logsource:
-                category: test_category
-                product: test_product
-            detection:
-                sel1:
-                    fieldA: value1
-                sel2:
-                    fieldB: value2
-                condition: 1 of sel*
-        """
-    )
-    assert elastalert_backend.convert(rule)[0] == (
-        """description: 
-index: *
-filter:
-- query:
-    query_string:
-      query: fieldA:value1 OR fieldB:value2
-priority: 1"""
-    )
-
 
 def test_elastalert_temporal_correlation_rule(elastalert_backend: ElastalertBackend):
     correlation_rule = SigmaCollection.from_yaml(
@@ -284,6 +205,36 @@ correlation:
     )
     with pytest.raises(NotImplementedError):
         elastalert_backend.convert(correlation_rule)
+
+
+def test_elastalert_temporal_ordered_correlation_rule(elastalert_backend: ElastalertBackend):
+    correlation_rule = SigmaCollection.from_yaml(
+        """
+title: Base rule 1
+name: base_rule_1
+status: test
+logsource:
+    category: test
+detection:
+    selection:
+        fieldA: value1
+        fieldB: value2
+    condition: selection
+---
+title: Temporal correlation rule
+status: test
+correlation:
+    type: temporal_ordered
+    rules:
+        - base_rule_1
+    group-by:
+        - fieldC
+    timespan: 15m
+"""
+    )
+    with pytest.raises(NotImplementedError):
+        elastalert_backend.convert(correlation_rule)
+
 
 def test_elastalert_multi_rule_query(elastalert_backend: ElastalertBackend):
     correlation_rule = SigmaCollection.from_yaml(
