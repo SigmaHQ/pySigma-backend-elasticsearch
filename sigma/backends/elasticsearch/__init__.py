ba7d38006abd7db98d472da1698a37fdd6b7f2b1
frack113 <62423083+frack113@users.noreply.github.com>
2024-11-15 17:56:42 +0100

    Add ElastalertBackend

--- sigma/backends/elasticsearch/__init__.py
+++ sigma/backends/elasticsearch/__init__.py
@@ -1,3 +1,4 @@
 from .elasticsearch_lucene import LuceneBackend
 from .elasticsearch_eql import EqlBackend
 from .elasticsearch_esql import ESQLBackend
+from .elasticsearch_elastalert import ElastalertBackend
